9175d46ca18579b83ed8a32de996f43056c3602a
Julio kamke <kamke.j@gmail.com>
2016-03-30 11:14:43 -0300

    img

--- WasteNot/app/src/main/java/br/com/wastenot/wastenot/CardDetail.java
+++ WasteNot/app/src/main/java/br/com/wastenot/wastenot/CardDetail.java
@@ -2,12 +2,17 @@ package br.com.wastenot.wastenot;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class CardDetail extends AppCompatActivity {
     ImageView imgCard;
@@ -31,9 +36,41 @@ public class CardDetail extends AppCompatActivity {
 
     }
 
-    public void setImage(Bitmap bitmap){
-        imgCard.setImageBitmap(bitmap);
+   private class LoadImage extends AsyncTask<String, String,Bitmap> {
+
+        Bitmap bitmap;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+     /*   super.onPreExecute();
+        pDialog = new ProgressDialog(MainActivity.this);
+        pDialog.setMessage("Loading Image ....");
+        pDialog.show();*/
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... args) {
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream) new URL(args[0]).getContent());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+        protected void onPostExecute(Bitmap bitmap) {
+            if(bitmap != null){
+                imgCard.setImageBitmap(bitmap);
+            }else {
+                Log.d("bit", "ok");
+              //  Toast.makeText(this, "Image Does Not exist or Network Error", Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
     }
 
 }
+
 
