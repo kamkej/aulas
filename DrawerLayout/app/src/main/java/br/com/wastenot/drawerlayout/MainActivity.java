223c735d7ba0aa42dd17ea6994ba70200820460f
Julio <kamke.j@gmail.com>
2016-04-14 22:41:59 -0300

    local

--- DrawerLayout/app/src/main/java/br/com/wastenot/drawerlayout/MainActivity.java
+++ DrawerLayout/app/src/main/java/br/com/wastenot/drawerlayout/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
