3c44658f371b87dbcc3d92f6272b59b8f337607b
Julio <kamke.j@gmail.com>
2016-04-29 11:35:39 -0300

    list

--- WasteNot/app/src/main/java/br/com/wastenot/wastenot/WishListActivity.java
+++ WasteNot/app/src/main/java/br/com/wastenot/wastenot/WishListActivity.java
@@ -1,17 +1,17 @@
 package br.com.wastenot.wastenot;
 
+
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.ListView;
 
-public class WishListActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
+public class WishListActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
