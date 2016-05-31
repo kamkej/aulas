6e45329e143754b471a81646a683cdd0ce829b64
Julio <kamke.j@gmail.com>
2016-05-31 10:26:56 -0300

    s

--- WasteNot/app/src/main/java/br/com/wastenot/wastenot/MyDeckActivity.java
+++ WasteNot/app/src/main/java/br/com/wastenot/wastenot/MyDeckActivity.java
@@ -1,13 +1,19 @@
 package br.com.wastenot.wastenot;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
-public class MyDeckActivity extends AppCompatActivity {
+public class MyDeckActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,15 @@ public class MyDeckActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_deck);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,4 +41,32 @@ public class MyDeckActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_decks) {
+            Intent intent = (new Intent(this,MyDeckActivity.class));
+            startActivity(intent);
+            finish();
+
+        } else if (id == R.id.nav_have_list) {
+            Intent intent = (new Intent(this,HaveListActivity.class));
+            startActivity(intent);
+            finish();
+
+        } else if (id == R.id.nav_wanted_list) {
+            Intent intent = (new Intent(this,wantedList.class));
+            startActivity(intent);
+            finish();
+        }else if (id == R.id.nav_home){
+            Intent intent = (new Intent(this,MainActivity.class));
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
